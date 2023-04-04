7d54087d263e3b44eedae7c0eec4a6313a54d041
Dimitri Andre <dimitri@ymagyn.com>
2023-04-04 08:57:53 +0200

    counter minus and plus

--- src/Index.jsx
+++ src/Index.jsx
@@ -6,8 +6,8 @@ import Counter from "./components/Counter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <div style={{ backgroundColor: "black", color: "white", height:  "100vh"}}>
-        <Header />
+        <Header></Header>
         <hr style={{margin: 0}}></hr>
-        <Counter />
+        <Counter></Counter>
     </div>
 );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,21 +3,25 @@ import React from "react";
 class Counter extends React.Component {
     constructor(props) {
         super(props);
+        this.handleAttack=this.handleAttack.bind(this);
+        this.handleDefense=this.handleDefense.bind(this);
         this.state = {
-            count:0
-        }
+            count: 0,
+        };
     }
 
     handleAttack(){
-        alert("Attack clicked")
+        //alert("Attack clicked");
+        this.setState({ count: this.state.count + 1})
     }
     handleDefense(){
-        alert("Defense clicked")
+        //alert("Defense clicked");
+        this.setState({ count: this.state.count - 1})
     }
     render(){
         return (
             <div className="row text-white" style={{padding: "1em"}}>
-                <h1>Counter: {this.state}</h1>
+                <h1>Counter: {this.state.count}</h1>
                     <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
                     <button onClick={this.handleDefense} style={{width:"200px"}}>-1</button>
             </div>
